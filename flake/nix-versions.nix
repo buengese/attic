ab9be85be2c6094a22ababcf201258c1402259f0
buengese <buengese@protonmail.com>
2026-05-11 21:50:14 +0200

    chore: update some dependencies

--- flake/nix-versions.nix
+++ flake/nix-versions.nix
@@ -36,9 +36,8 @@ in
       attic.nix-versions = {
         versions = {
           default = pkgs.nix;
-          "2.24" = pkgs.nixVersions.nix_2_24;
-          "2.26" = pkgs.nixVersions.nix_2_26;
           "2.28" = pkgs.nixVersions.nix_2_28;
+          "2.31" = pkgs.nixVersions.nix_2_31;
         };
 
         manifestFile = let
